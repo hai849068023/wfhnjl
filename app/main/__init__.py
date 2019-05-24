4057c1f7d1dafe1fe284618a26d1e28c991deaf6
liuxu <18655109810@163.com>
2019-05-24 18:10:42 +0800

    delete

--- app/main/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'liuxu'
-__date__ = '2019/5/17 9:47'
-from flask import Blueprint
-
-main = Blueprint('main', __name__, template_folder='templates')
-
-from . import views
--- manager.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'liuxu'
-__date__ = '2019/5/18 9:27'
-from app.models import  manager
-
-
-if __name__ == '__main__':
-    manager.run()
